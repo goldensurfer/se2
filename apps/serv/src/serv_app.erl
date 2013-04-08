bb469ce5f3864ce9cd1e32f337ee5be340cce149
Paul Peregud <paulperegud@gmail.com>
2013-04-08 22:06:32 +0200

    fix tests

--- apps/serv/src/serv_app.erl
+++ apps/serv/src/serv_app.erl
@@ -5,23 +5,22 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-include_lib("serv/include/logging.hrl").
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
+%% note that all requrements from app.src file should be started 
+%% before calling application:start(serv).
 start(_StartType, _StartArgs) ->
-    application:start(gproc, permanent),
-    application:start(lager, permanent),
-    application:start(ranch, permanent),
     Res = serv_sup:start_link(),
     TcpOpts = [{mode, list}, {port, 1090}, {active, true}],
-    ranch:start_listener(gm_socket, 1, ranch_tcp, TcpOpts, stream_parser, []),
+    ranch:start_listener(play_socket, 1, ranch_tcp, TcpOpts, stream_parser, []),
     Res.
 
 stop(_State) ->
-    ranch:stop_listener(gm_socket),
+    ranch:stop_listener(play_socket),
     serv_sup:stop(),
-    application:stop(ranch),
-    application:stop(lager),
-    application:stop(gproc),
     ok.
+
