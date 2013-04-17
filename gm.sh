560eee57f0dfb214c10f21f9b5fde8fc67eea8bc
Paul Peregud <paulperegud@gmail.com>
2013-04-17 16:36:20 +0200

    server handles most of the messages
    gm_client sends some of the protocol messages and accepts most of them

--- gm.sh
+++ gm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# ENV="ERL_CRASH_DUMP_SECONDS 1" -env $ENV
-OPTS_COMMON="-pa apps/*/ebin -pa deps/*/ebin -boot start_sasl"
+ENV="ERL_CRASH_DUMP_SECONDS 1"
+
+OPTS_COMMON="-pa apps/*/ebin -pa deps/*/ebin -env $ENV -boot start_sasl"
 
 usage() {
     echo usage: `basename $0` HOST PORT ID [GAMETYPE]
@@ -14,8 +15,8 @@ fi
 
 if [ -z "$4" ] ; then
     echo "start 3"
-    # erl "$OPTS_COMMON" -eval "gm_client:start_link(\"$1\", $2, $3)."
+    echo erl "$OPTS_COMMON" -eval "gm_client:start_link(\"$1\", $2, $3)."
 else
     echo "start 4"
-    # erl "$OPTS_COMMON" -eval "gm_client:start_link(\"$1\", $2, $3, \"$4\")."
+    echo erl "$OPTS_COMMON" -eval "gm_client:start_link(\"$1\", $2, $3, \"$4\")."
 fi
