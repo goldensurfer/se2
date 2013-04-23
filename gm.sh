70709027db26e4517a936c6667b046b1f7418f63
Paul Peregud <paulperegud@gmail.com>
2013-04-23 07:40:28 +0200

    fix gm.sh

--- gm.sh
+++ gm.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-ENV="ERL_CRASH_DUMP_SECONDS 1"
-
-OPTS_COMMON="-pa apps/*/ebin -pa deps/*/ebin -env $ENV -boot start_sasl"
+OPTS_COMMON="-pa apps/*/ebin -pa deps/*/ebin -env ERL_CRASH_DUMP_SECONDS 1 -boot start_sasl"
 
 usage() {
     echo usage: `basename $0` HOST PORT ID [GAMETYPE]
@@ -15,8 +13,8 @@ fi
 
 if [ -z "$4" ] ; then
     echo "start 3"
-    echo erl "$OPTS_COMMON" -eval "gm_client:start_link(\"$1\", $2, $3)."
+    erl $OPTS_COMMON -eval "gm_client:start_link(\"$1\", $2, $3)."
 else
     echo "start 4"
-    echo erl "$OPTS_COMMON" -eval "gm_client:start_link(\"$1\", $2, $3, \"$4\")."
+    erl $OPTS_COMMON -eval "gm_client:start_link(\"$1\", $2, $3, \"$4\")."
 fi
